9efe61ff2e43d83e632fb00742fc9e0c4641dad2
p.h <109684733+schlachtergeselle@users.noreply.github.com>
2022-08-03 18:15:38 +0200

    Add files via upload

---
+++ Files/Business card/src/components/interests.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default function Interests (){
+
+    return (
+    <interests>
+        
+            <h1>Other interests</h1>
+                <p>
+                    The question should rather be, what I am not interested in and what interests I pursue, 
+                    since I'm curious about almost anything, be it engineering, music, history, phsyics, medicine or biology. 
+                </p>
+                <p>
+                    On the other hand, insurances, bureaucracy and fashion are things I don't care about.
+                </p>
+
+       
+    </interests>
+    )
+}
